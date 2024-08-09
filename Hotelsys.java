2ffa80eeda91798d27b1fe965342381dfa4e0c7f
Mowthik <135473774+mowax31@users.noreply.github.com>
2024-08-09 18:46:52 +0530

    Add files via upload

---
+++ Hotelsys.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hotelsys;
+
+/**
+ *
+ * @author Mowthik
+ */
+
+
+public class Hotelsys {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       login_ui lgin = new login_ui();
+       lgin.show();
+        
+     }
+    }
